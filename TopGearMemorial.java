89fd43e02b626a522c645e74aad9475c590b76bc
gabrieledecarvalho <carvalhodegabriele@gmail.com>
2025-02-22 13:40:19 -0300

    chamando o constructor do JFrame para o título do jogo

--- TopGearMemorial.java
+++ TopGearMemorial.java
@@ -5,8 +5,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-class TopGearMemorial extends JFrame implements KeyListener, ActionListener {
+
+// Classe herdando do JFrame e (depois) implementando as interfaces KeyListener (para eventos do teclado) e o actionListener para eventos 
+class TopGearMemorial extends JFrame /*implements KeyListener, ActionListener */{
+    
+    // Constructor: iníciando o jogo
+    public TopGearMemorial(String title){
+        // Chamando o constructor do JFrame
+        super(title);
+    }
     public static void main(String[] args) {
-        
+        TopGearMemorial game = new TopGearMemorial("Top Gear Memorial");
     }
 }
